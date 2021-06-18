a5dfa0b76127dd6bf91130de29ac90e76b73e93a
mayank698 <mpandeymp0@gmail.com>
2021-06-18 10:55:32 +0530

    .

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const api_config={
-    port: 5000 || process.env.PORT,
+    port: process.env.PORT,
     db_url: 'mongodb+srv://Mayank:mayank123@mayank.qddo3.mongodb.net/reactDB?retryWrites=true&w=majority'
 }
 module.exports=api_config;
